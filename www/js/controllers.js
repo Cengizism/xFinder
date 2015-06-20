5cf9e1e3fa21e907ebfddfea2fe201f585679ba3
Cengizism <culusoy@ask-cs.com>
2015-06-20 20:04:58 +0200

    google maps working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,8 +3,8 @@
 angular.module('starter.controllers', [])
 
   .controller('LocationsCtrl', [
-    '$rootScope', '$scope', 'Chats', 'Location',
-    function ($rootScope, $scope, Chats, Location) {
+    '$rootScope', '$scope', 'Location',
+    function ($rootScope, $scope, Location) {
       // With the new view caching in Ionic, Controllers are only called
       // when they are recreated or on app start, instead of every page change.
       // To listen for when this page is active (for example, to refresh data),
@@ -93,8 +93,65 @@ angular.module('starter.controllers', [])
 
     }])
 
-  .controller('LocationDetailCtrl', function ($rootScope, $scope, $stateParams) {
+  .controller('LocationDetailCtrl', function ($rootScope, $scope, $stateParams, $ionicLoading, $compile) {
     $scope.location = $rootScope.results[$stateParams.locationId];
+
+
+    $scope.init = function () {
+      var myLatlng = new google.maps.LatLng(43.07493, -89.381388);
+
+      var mapOptions = {
+        center: myLatlng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById("map"),
+        mapOptions);
+
+      //Marker + infowindow + angularjs compiled ng-click
+      var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+      var compiled = $compile(contentString)($scope);
+
+      var infowindow = new google.maps.InfoWindow({
+        content: compiled[0]
+      });
+
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Uluru (Ayers Rock)'
+      });
+
+      google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+      });
+
+      $scope.map = map;
+    };
+
+    // google.maps.event.addDomListener(window, 'load', initialize);
+
+    $scope.centerOnMe = function () {
+      if (!$scope.map) {
+        return;
+      }
+
+      $scope.loading = $ionicLoading.show({
+        content: 'Getting current location...',
+        showBackdrop: false
+      });
+
+      navigator.geolocation.getCurrentPosition(function (pos) {
+        $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        $scope.loading.hide();
+      }, function (error) {
+        alert('Unable to get location: ' + error.message);
+      });
+    };
+
+    $scope.clickTest = function () {
+      alert('Example of infowindow with ng-click')
+    };
   })
 
   .controller('SettingsCtrl', function ($scope) {
